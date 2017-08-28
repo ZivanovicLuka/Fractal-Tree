ca27d08c0171e499d69e3ee8dc2f29ac7b29ead4
Luka Zivanovic <lukazivanovic1995@gmail.com>
2017-08-28 02:52:02 +0200

    Randomness, bg

--- script.js
+++ script.js
@@ -1,54 +1,83 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-c.width  = window.innerWidth - 30;
+c.width = window.innerWidth - 30;
 c.height = window.innerHeight - 10;
 
-function color(width){
-  red = Math.ceil(235*(width/start_width)).toString(16);
+var len;
+var start_width;
+var left_rotation;
+var left_rotation;
+var left_length;
+var right_length;
+var randomness;
+
+var end_color = '#BA3636';
+
+function color(width) {
+  red = Math.ceil(235 * (width / start_width)).toString(16);
   red = red.length == 1 ? "0" + red : red;
   green = 'aa';
-  blue = Math.floor(130*(width/start_width)).toString(16);
+  blue = Math.floor(130 * (width / start_width)).toString(16);
   blue = blue.length == 1 ? "0" + blue : blue;
-  ctx.strokeStyle = '#'+red+green+blue;
+  ctx.strokeStyle = '#' + red + green + blue;
 }
 
-function branch(length, startX, startY, width, rotate){
+function leaf(endX, endY, length) {
+  ctx.arc(endX, endY, length / 4, 0, 2 * Math.PI, false);
+  ctx.fillStyle = end_color;
+  ctx.fill();
+}
 
-  var endX = Math.cos(rotate)*length + startX;
-  var endY = Math.sin(rotate)*length + startY;
+function branch(length, startX, startY, width, rotate) {
+  rotate = rotate + ((Math.random() * Math.PI - Math.PI / 2) * randomness / 100) / width * 2;
+
+  old_len = length;
+  length *= (Math.random() - .5) * randomness / 100 * 4;
+  length += old_len;
+
+  if (length < old_len / 2)
+    length = old_len;
+
+  var endX = Math.cos(rotate) * length + startX;
+  var endY = Math.sin(rotate) * length + startY;
 
   color(width);
 
   ctx.beginPath();
-  ctx.moveTo(startX,startY);
-  ctx.lineTo(endX,endY);
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
   ctx.lineWidth = width;
 
   ctx.closePath();
   ctx.stroke();
 
-  if(width>1){
-    branch(length*left_length,endX,endY,width-1,rotate - left_rotation);
-    branch(length*right_length,endX,endY,width-1,rotate + right_rotation);
-  } else {
-    ctx.arc(endX, endY, length/4, 0, 2 * Math.PI, false);
-    ctx.fillStyle = end_color;
-    ctx.fill();
+  let has_branch = false;
+
+  if (width > 1) {
+    length = old_len;
+    if (randomness * Math.random() < 75) {
+      branch(length * left_length, endX, endY, width - 1, rotate - left_rotation);
+      has_branch = true;
+    }
+    if (randomness * Math.random() > 45) {
+      branch(length * left_length, endX, endY, width - 1, rotate);
+      has_branch = true;
+    }
+    if (randomness * Math.random() < 75) {
+      branch(length * right_length, endX, endY, width - 1, rotate + right_rotation);
+      has_branch = true;
+    }
+  }
+
+  if (!has_branch) {
+    leaf(endX, endY, length);
   }
 }
 
-
-var len = document.getElementById("len").value;
-var start_width = document.getElementById("width").value;
-var left_rotation = document.getElementById("lr").value / 180 * Math.PI;
-var left_rotation = document.getElementById("rr").value / 180 * Math.PI;
-var left_length = document.getElementById("ll").value / 10;
-var right_length = document.getElementById("rl").value / 10;
-var end_color = 'red';
-
-function draw_tree(){
+function draw_tree() {
   ctx.clearRect(0, 0, c.width, c.height);
+
 
   len = document.getElementById("len").value;
   start_width = document.getElementById("width").value;
@@ -56,8 +85,9 @@ function draw_tree(){
   right_rotation = document.getElementById("rr").value / 180 * Math.PI;
   left_length = document.getElementById("ll").value / 10;
   right_length = document.getElementById("rl").value / 10;
+  randomness = document.getElementById("randomness").value;
 
-  branch(len,c.width/2,c.height,start_width,-Math.PI/2);
+  branch(len, c.width / 2, c.height, start_width, -Math.PI / 2);
 }
 
 draw_tree();
